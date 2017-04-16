7c8ecd86f84a17fe766223354dbdec7d85af5f5a
wang.weichen <weichen.wang@jsxforg.com>
2017-04-16 15:18:13 +0800

    Import __all__

--- weibopy/__init__.py
+++ weibopy/__init__.py
@@ -2,12 +2,19 @@
 
 from .auth import WeiboAuth
 from .comment import WeiboComments
-from .exceptions import WeiboAPIError
+from .exceptions import WeiboAPIError, WeiboOauth2Error
 from .relation import WeiboRelation
 from .services import WeiboServices
 from .tweet import WeiboTweet
 from .user import WeiboUser
 
-
-
-
+__all__ = [
+    "WeiboAuth",
+    "WeiboComments",
+    "WeiboAPIError",
+    "WeiboOauth2Error",
+    "WeiboRelation",
+    "WeiboServices",
+    "WeiboTweet",
+    "WeiboUser"
+]
