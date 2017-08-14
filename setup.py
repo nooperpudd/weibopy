36f55112def3a3a16e1d86c35c6a71c79b7d8749
wangweichen <weichen.wang@jsxfedu.com>
2017-08-14 10:19:00 +0800

    fix python setup encoding error.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_version(*file_paths):
     here = os.path.abspath(os.path.dirname(__file__))
 
     # Use codecs.open for Python 2 compatibility
-    with codecs.open(os.path.join(here, *file_paths), 'r') as f:
+    with codecs.open(os.path.join(here, *file_paths), 'r', encoding="utf-8") as f:
         version_file = f.read()
 
     # The version line must have the form
@@ -28,29 +28,28 @@ def find_version(*file_paths):
 setup(
     name="weibopy",
     version=find_version("weibopy", "__init__.py"),
-    description="Weibo API SDK",
-    long_description=open("README.md").read(),
+    description="Sina Weibo API SDK",
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
     author="Winton Wang",
     url="https://coding.net/u/wefindx/p/weibopy/git",
     license="BSD",
     author_email="365504029@qq.com",
     packages=find_packages(exclude=['*tests*']),
-    install_requires=["requests<=2.11.1"], # todo httpretty compatibility with requests 2.13.0
+    install_requires=["requests>=2.18.1"],  # todo httpretty compatibility with requests 2.13.0
     tests_require=["pytest",
                    "HTTPretty"],
     zip_safe=False,
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only"
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Environment :: Web Environment",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment"
     ]
 
 )
